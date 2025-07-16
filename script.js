1768ba20e2de954173eb86af2ed0b3258bb22a78
glngprr <gilangpermadi692@gmail.com>
2025-07-17 06:37:30 +0700

    fix error nav

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const mobileToggle = document.getElementById('mobileToggle');
 const navMenu = document.getElementById('navMenu');
+const iconMenu = document.getElementById('iconMenu');
+const iconClose = document.getElementById('iconClose');
 
 mobileToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-    mobileToggle.textContent = navMenu.classList.contains('active') ? '✕' : '☰';
+navMenu.classList.toggle('active');
+
+const menuIsActive = navMenu.classList.contains('active');
+    iconMenu.classList.toggle('hidden', menuIsActive);
+    iconClose.classList.toggle('hidden', !menuIsActive);
 });
